1d9f7a05c93b4e467a25b9a166ebe463dca19b5c
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-02-14 12:27:03 +0600

    leared the basics of file based routing

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>The page not found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,11 +2,22 @@ import { useRouter } from "next/router";
 
 const ClientProjectsPage = () => {
   const router = useRouter();
-  console.log("Client id", router.query);
+
+  const loadProjectHandler = () => {
+    console.log(router);
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: router.query?.id,
+        clientprojectid: "projectA",
+      },
+    });
+  };
 
   return (
     <div>
-      <h1>The projects of a {router.query?.id}!</h1>
+      <h1>The projects of {router.query?.id}!</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 };
